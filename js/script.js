366222e88efa72c63abce72a2f394ce79d8f20d3
Steven-Blombou <blombouteven@gmail.com>
2020-03-17 17:28:15 +0100

    Commit num2

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//Réaliser un script js permettant d'afficher un formulaire avec un texte avec écris à l'intérieur "Je suis une petite perruche"
+
+document.write("<form><input value='Je suis une petite perruche'></form>");
